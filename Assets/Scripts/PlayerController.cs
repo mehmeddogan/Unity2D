5e3cc102e037c227fe5f65e849aacd469cf53511
mehmeddogan <mehmetdogan5324@gmail.com>
2021-04-02 22:32:18 +0300

    --

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D playerRB;
+    Animator playerAnimator;
+    public float moveSpeed = 1f;
+    public float jumpSpeed = 1f, jumpFrequincy=1f, nextJumpTime=0f;
+    bool facingRight = true;
+    public bool isGrounded = false;
+    public Transform groundCheckPosition;
+    public float groundCheckRadius;
+    public LayerMask groundCheckLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = this.GetComponent<Rigidbody2D>();
+        playerAnimator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalMove();
+        OnGroundCheck();
+        
+
+        if (playerRB.velocity.x < 0 && facingRight)
+        {
+            FlipFace();
+        }
+        else if (playerRB.velocity.x > 0 && !facingRight)
+        {
+            FlipFace();
+        }
+        if (Input.GetAxis("Vertical") > 0 && isGrounded && (nextJumpTime<Time.timeSinceLevelLoad))
+        {
+            nextJumpTime = Time.timeSinceLevelLoad + jumpFrequincy;
+            Jump();
+        }
+    }
+    private void FixedUpdate()
+    {
+
+    }
+    void HorizontalMove()
+    {
+        playerRB.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, playerRB.velocity.y);
+        playerAnimator.SetFloat("PlayerSpeed", Mathf.Abs(playerRB.velocity.x));
+    }
+    void FlipFace()
+    {
+        facingRight = !facingRight;
+        Vector3 tempLocalScale = transform.localScale;
+        tempLocalScale.x *= -1;
+        transform.localScale = tempLocalScale;
+    }
+    void Jump()
+    {
+        playerRB.AddForce(new Vector2(0f, jumpSpeed));
+    }
+    void OnGroundCheck()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckPosition.position, groundCheckRadius, groundCheckLayer);
+        playerAnimator.SetBool("isGroundedAnim", isGrounded);
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerManager : MonoBehaviour
+{
+    public float health;
+    bool dead = false;
+    // Start is called before the first frame update
+    //childın transformuna ulasmaya calısıyorum
+    Transform muzzle;
+    public Transform bullet, floatingText;
+    public float bulletSpeed;
+
+    public Slider slider;
+    
+    void Start()
+    {
+        muzzle = transform.GetChild(1);
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            ShootBullet();
+        }
+    }
+    public void GetDamage(float damage)
+    {
+        Instantiate(floatingText, transform.position, Quaternion.identity).GetComponent<TextMesh>().text = damage.ToString();
+        if((health-damage) >=0)
+        {
+            health -= damage;
+        }
+        else
+        {
+            health = 0;
+        }
+        slider.value = health;
+        AmIDead();
+    }
+    void AmIDead()
+    {
+        if(health<=0)
+        {
+            dead = true;
+        }
+    }
+    void ShootBullet()
+    {
+        Transform tempBullet; // her mermiye gecici süre ulasmak için
+        tempBullet = Instantiate(bullet, muzzle.position, Quaternion.identity);
+        tempBullet.GetComponent<Rigidbody2D>().AddForce(muzzle.forward * bulletSpeed);//forward burada z ekseni oldugu için sorun yasadık mermi gitmedi bu yüzden oyunu 3d yapıp z eksenini düzeltiyoruz
+
+    }
+}
